52f990a37207505a47b305e8bcc0a774b3af39b4
John Cummins <John C>
2024-01-01 20:23:38 +0000

    few updates. mostly mess

--- Scrapey.py
+++ Scrapey.py
@@ -9,8 +9,16 @@ layer1 = soup.find_all('div','games-list-item')
 # print(soup.prettify())
 # print(layer1)
 layer2 = soup.find_all('div', 'games')
-# print(layer2)
+# print(layer2[10].text)
 layer3 = soup.find_all('div', 'game scores')
 # print(layer3)
 layer4 = soup.find_all('div', 'venue')
-print(layer4[0].text)
+# layer5 = layer3.find_all('div', 'venue')
+# print(layer3.find('div', 'venue'))
+# print(layer3.text)
+layer5 = soup.find_all("class", "data-optaid")
+print(layer5)
+
+# for item in layer2:
+#     print(item)
+
